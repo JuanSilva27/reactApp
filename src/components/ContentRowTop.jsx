9dd8d98bab85e43ee4d885654c0fb090717987ff
JuanSilva27 <juan.silva.0270@gmail.com>
2022-02-07 23:24:40 -0300

    se modulariza el resto de componentes

--- src/components/ContentRowTop.jsx
+++ src/components/ContentRowTop.jsx
@@ -1,5 +1,7 @@
-import mandalorian from "../assets/img/mandalorian.jpg"
 import ContentRowMovies from "./ContentRowMovies"
+import GenresInDb from "./GenresInDb"
+import LastMovieInDb from "./LastMovieInDb"
+
 
 function ContentRowTop(){
     const datos=[
@@ -22,6 +24,10 @@ function ContentRowTop(){
             icono:"fas fa-user fa-2x text-gray-300"
         }
     ]
+
+    const genres =["Acción","Animación","Aventura","Ciencia Ficción","Comedia","Documental","Drama","Fantasia","Infantiles","Musical"]
+    
+
     return(
         <div className="container-fluid">
         <div className="d-sm-flex align-items-center justify-content-between mb-4">
@@ -32,103 +38,9 @@ function ContentRowTop(){
         <ContentRowMovies datos={datos} />
 
         <div className="row">
-            <div className="col-lg-6 mb-4">
-                <div className="card shadow mb-4">
-                    <div className="card-header py-3">
-                        <h5 className="m-0 font-weight-bold text-gray-800">Last movie in Data Base</h5>
-                    </div>
-                    <div className="card-body">
-                        <div className="text-center">
-                            <img className="img-fluid px-3 px-sm-4 mt-3 mb-4" style={{width: "40rem"}} src={mandalorian} alt=" Star Wars - Mandalorian "/>
-                        </div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, consequatur explicabo officia inventore libero veritatis iure voluptate reiciendis a magnam, vitae, aperiam voluptatum non corporis quae dolorem culpa citationem ratione aperiam voluptatum non corporis ratione aperiam voluptatum quae dolorem culpa ratione aperiam voluptatum?</p>
-                        <a className="btn btn-danger" target="_blank" rel="nofollow" href="/">View movie detail</a>
-                    </div>
-                </div>
-            </div>
+            <LastMovieInDb/>
+            <GenresInDb genres={genres}/>
 
-            <div className="col-lg-6 mb-4">						
-                <div className="card shadow mb-4">
-                    <div className="card-header py-3">
-                        <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
-                    </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Acción
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Animación
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Aventura
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Ciencia Ficción
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Comedia
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Documental
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Drama
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Fantasia
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Infantiles
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Musical
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
     )
---
+++ src/components/GenresInDb.jsx
@@ -0,0 +1,25 @@
+function GenresInDb(props) {
+    return (
+        <div className="col-lg-6 mb-4">
+            <div className="card shadow mb-4">
+                <div className="card-header py-3">
+                    <h5 className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h5>
+                </div>
+                <div className="card-body">
+                    <div className="row">
+                        {props.genres.map((prop, i) =>
+                            <div key={prop+i} className="col-lg-6 mb-4">
+                                <div className="card bg-dark text-white shadow">
+                                    <div className="card-body">
+                                        {prop}
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default GenresInDb
